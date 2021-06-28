ab17565fdbdddc5bf2c56cc989485c0818f5b7b5
amEhLee <E.kabe2324@gmail.com>
2021-06-28 12:35:56 -0600

    possible way to run tests?
    - reconfigured TestSort to be independent of anything outside of args
    - appdriver revised to start the interaction

--- src/testSorts/TestSort.java
+++ src/testSorts/TestSort.java
@@ -4,6 +4,21 @@ import comparison.ShapeUtil;
 import shapes.Shape;
 
 public class TestSort {
+
+    public TestSort(String[] args) {
+        argParser(args);
+    }
+
+    public TestSort(String infile, String compareBy, String sortType) {
+        Shape[] givenArray = ShapeUtil.readFile(infile);
+
+        // convert from char format to string
+        compareBy = convertCompare(compareBy);
+        sortType = convertSortType(sortType);
+
+        runTest(givenArray, sortType, compareBy);
+    }
+
     protected static void runTest(Shape[] array, String algorithm, String sort) {
         long startTime;
         long endTime;
@@ -13,8 +28,11 @@ public class TestSort {
             shapeutil = new ShapeUtil(0);
         } else if(sort.equals("volume")) {
             shapeutil = new ShapeUtil(2);
-        } else {
+        } else if (sort.equals("base")) {
             shapeutil = new ShapeUtil(1);
+        } else {
+            // default to height
+            shapeutil = new ShapeUtil(0);
         }
 
         startTime = System.currentTimeMillis();
@@ -52,6 +70,71 @@ public class TestSort {
 
         System.out.println("-------------------------------------------");
         System.out.println("It took " + (endTime - startTime) + " ms.");
+    }
+
+    public static void argParser(String[] args) {
+		String file = "", 
+			   sortType = "", 
+			   compareType = "";
+  
+		for (String a : args) {
+  
+		   switch (Character.toLowerCase(a.charAt(0))) {
+			  case 'f':
+				 file = a.substring(1);
+				 break;
+			  case 's':
+				 sortType = String.valueOf(Character.toLowerCase(a.charAt(1)));
+				 break;
+			  case 't':
+				 compareType = String.valueOf(Character.toLowerCase(a.charAt(1)));
+				 break;
+		   
+			  default:
+				 System.out.printf("argument %s cannot be read \n", Character.toLowerCase(a.charAt(0)));
+				 break;
+		   }
+		   
+		}
+		
+        // create new object with update params
+        TestSort t = new TestSort(file, compareType, sortType);
+	}
+
+    public static String convertSortType(String given){
+        switch (given) {
+
+            // convert sort types
+            case "b": given = "Bubble";
+            case "s": given = "Selection";
+            case "i": given = "Insertion";           
+            case "m": given = "Merge";
+            case "q": given = "Quick";
+            case "z": given = "Chosen";
+
+            default:
+                System.out.println("char is not any of the given sort types");
+                break;
+        }
+
+        return given;
+        
+    }
+
+    public static String convertCompare(String given) {
+        switch (given) {
+
+            // convert compare by's
+            case "v": given = "volume";
+            case "a": given = "base";
+            case "h": given = "height";
+
+            default:
+                System.out.println("char is not any of the given compare types");
+                break;
+        }
+
+        return given;
     }
 
 }
