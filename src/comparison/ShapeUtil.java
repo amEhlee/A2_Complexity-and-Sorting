03ae12a34b3081abd71ce98fc92cc18b2f6f4ebd
Jake Jeon <22580231+Jake-Je0n@users.noreply.github.com>
2021-06-28 01:49:54 -0600

    TestInsertionSort & insertionSort()
    made it sort in descending order

--- src/comparison/ShapeUtil.java
+++ src/comparison/ShapeUtil.java
@@ -93,7 +93,7 @@ public class ShapeUtil implements Comparator {
             /* Move elements of arr[0..i-1], that are
                greater than key, to one position ahead
                of their current position */
-				while (j >= 0 && compare(array[j], key) > 0) {
+				while (j >= 0 && compare(array[j], key) < 0) {
 					array[j + 1] = array[j];
 					j = j - 1;
 				}
---
+++ src/testSorts/TestInsertionSort.java
@@ -0,0 +1,53 @@
+package testSorts;
+
+import comparison.ShapeUtil;
+import shapes.Shape;
+
+public class TestInsertionSort {
+
+    public static void main(String[] args) {
+        // Testing Start
+        ShapeUtil byHeight = new ShapeUtil(0);
+        ShapeUtil byArea = new ShapeUtil(1);
+        ShapeUtil byVolume = new ShapeUtil(2);
+        long startTime = 0;
+        long endTime = 0;
+
+
+        Shape[] array = byArea.readFile("res/polyfor1.txt");
+
+        startTime = System.currentTimeMillis();
+        byHeight.insertionSort(array);
+        endTime = System.currentTimeMillis();
+        System.out.println("\nBubble Sort by height:");
+        for(int i = 0; i < array.length; i+=1000) {
+            System.out.println("[" + i + "] " + array[i].getHeight());
+        }
+        System.out.println("[" + (array.length - 1) + "] " + array[(array.length - 1)].getHeight());
+        System.out.println("-------------------------------------------");
+        System.out.println("It took " + (endTime - startTime) + " ms.");
+
+        startTime = System.currentTimeMillis();
+        byArea.insertionSort(array);
+        endTime = System.currentTimeMillis();
+        System.out.println("\nBubble Sort by base area:");
+        for(int i = 0; i < array.length; i+=1000) {
+            System.out.println("[" + i + "] " + array[i].calcBaseArea());
+        }
+        System.out.println("[" + (array.length - 1) + "] " + array[(array.length - 1)].calcBaseArea());
+        System.out.println("-------------------------------------------");
+        System.out.println("It took " + (endTime - startTime) + " ms.");
+
+        startTime = System.currentTimeMillis();
+        byVolume.insertionSort(array);
+        endTime = System.currentTimeMillis();
+        System.out.println("\nBubble Sort by volume:");
+        for(int i = 0; i < array.length; i+=1000) {
+            System.out.println("[" + i + "] " + array[i].calcVolume());
+        }
+        System.out.println("[" + (array.length - 1) + "] " + array[(array.length - 1)].calcVolume());
+        System.out.println("-------------------------------------------");
+        System.out.println("It took " + (endTime - startTime) + " ms.");
+        // Testing End
+    }
+}
