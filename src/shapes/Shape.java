e7b0acb274255472625e92d90bafb487db5a1a1a
Jake Jeon <22580231+Jake-Je0n@users.noreply.github.com>
2021-07-01 15:31:52 -0600

    Javaodc added

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,12 +1,40 @@
 package shapes;
 
+/**
+ * Abstract class for all the shapes
+ *
+ * @author Don Laliberte
+ * @author Elie Kabengele
+ * @author Jared Smith
+ * @author Jaekyung Jeon
+ * @version June 30th, 2021
+ */
 public abstract class Shape implements Comparable {
 
     private double height;
 
+    /**
+     * Calculates the base area of the shape and return the value
+     * @return Calculated base area
+     */
     public abstract double calcBaseArea();
+
+    /**
+     * Calculates the volume of the shape and return the value
+     * @return Calculated volume
+     */
     public abstract double calcVolume();
+
+    /**
+     * Sets the height of the shape
+     * @param height height to be set as
+     */
     public abstract void setHeight(double height);
+
+    /**
+     * Gets the height of the shape
+     * @return height of the shape
+     */
     public abstract double getHeight();
 
 }
